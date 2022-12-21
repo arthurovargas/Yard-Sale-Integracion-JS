a6694b72572138a7351fca424c0906ab6a19329e
ArturoVargas <sergiovargas199791@gmail.com>
2022-12-21 13:44:09 -0500

    Mobile menu integration

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopEmailMenu  = document.querySelector('.desktop-menu');
+const iconMobileMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+
 
 navEmail.addEventListener('click', showEmailMenu);
+iconMobileMenu.addEventListener('click', showMobileMenu)
 
 function showEmailMenu(){
     desktopEmailMenu.classList.toggle('innactive');
+}
+
+function showMobileMenu(){
+    mobileMenu.classList.toggle('innactive');
 }
