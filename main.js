e0538e9f25046aa76dd7329e7af9b2759e7ddcec
ArturoVargas <sergiovargas199791@gmail.com>
2022-12-21 22:08:53 -0500

    Product cart detail integration

--- main.js
+++ main.js
@@ -2,15 +2,25 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopEmailMenu  = document.querySelector('.desktop-menu');
 const iconMobileMenu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
-
+const productCartDetail = document.querySelector('.product-cart-detail')
+const navbarshoppingCart = document.querySelector('.navbar-shopping-cart')
 
 navEmail.addEventListener('click', showEmailMenu);
 iconMobileMenu.addEventListener('click', showMobileMenu)
+navbarshoppingCart.addEventListener('click', showProductCartDetail)
 
 function showEmailMenu(){
+    productCartDetail.classList.add('innactive')
     desktopEmailMenu.classList.toggle('innactive');
 }
 
 function showMobileMenu(){
     mobileMenu.classList.toggle('innactive');
+    productCartDetail.classList.add('innactive');
+}
+
+function showProductCartDetail(){
+    desktopEmailMenu.classList.add('innactive');
+    mobileMenu.classList.add('innactive');
+    productCartDetail.classList.toggle('innactive');
 }
